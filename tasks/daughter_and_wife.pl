2e071f6792439e54f28f8e7fbd0a3112e5fa8a69
Романов Владислав <127548897+RomanovVladislav2003@users.noreply.github.com>
2024-03-05 15:38:36 +0300

    Загрузил задание 1 из ЛР 1

---
+++ tasks/daughter_and_wife.pl
@@ -0,0 +1,61 @@
+man(voeneg).
+man(ratibor).
+man(boguslav).
+man(velerad).
+man(duhovlad).
+man(svyatoslav).
+man(dobrozhir).
+man(bogomil).
+man(zlatomir).
+
+woman(goluba).
+woman(lubomila).
+woman(bratislava).
+woman(veslava).
+woman(zhdana).
+woman(bozhedara).
+woman(broneslava).
+woman(veselina).
+woman(zdislava).
+
+parent(voeneg,ratibor).
+parent(voeneg,bratislava).
+parent(voeneg,velerad).
+parent(voeneg,zhdana).
+
+parent(goluba,ratibor).
+parent(goluba,bratislava).
+parent(goluba,velerad).
+parent(goluba,zhdana).
+
+parent(ratibor,svyatoslav).
+parent(ratibor,dobrozhir).
+parent(lubomila,svyatoslav).
+parent(lubomila,dobrozhir).
+
+parent(boguslav,bogomil).
+parent(boguslav,bozhedara).
+parent(bratislava,bogomil).
+parent(bratislava,bozhedara).
+
+parent(velerad,broneslava).
+parent(velerad,veselina).
+parent(veslava,broneslava).
+parent(veslava,veselina).
+
+parent(duhovlad,zdislava).
+parent(duhovlad,zlatomir).
+parent(zhdana,zdislava).
+parent(zhdana,zlatomir).
+
+%Сhecks if X is a daughter of Y
+daughter(X,Y):- woman(X), parent(Y,X).
+
+%The predicate that outputs the daughter of X
+daughter(X):- daughter(Y,X), parent(X,Y), print(Y), nl, fail.
+
+%Checks if X is Y wife
+wife(X,Y):- woman(X), man(Y), parent(X,K), parent(Y,K), write(yes), !.
+
+%The predicate that outputs the wife of X
+wife(X):- man(X), parent(X,Z), parent(Y,Z), woman(Y), write(Y), nl, !.
