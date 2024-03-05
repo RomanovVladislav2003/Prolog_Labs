cb38909132bda0f2578874f0460eb5d4fda2a09b
Романов Владислав <127548897+RomanovVladislav2003@users.noreply.github.com>
2024-03-05 15:40:46 +0300

    Добавил задание 2 из ЛР1

--- tasks/daughter_and_wife.pl
+++ tasks/daughter_and_wife.pl
@@ -59,3 +59,26 @@ wife(X,Y):- woman(X), man(Y), parent(X,K), parent(Y,K), write(yes), !.
 
 %The predicate that outputs the wife of X
 wife(X):- man(X), parent(X,Z), parent(Y,Z), woman(Y), write(Y), nl, !.
+
+%Сhecks if X is the son of Y
+son(X,Y):- man(X), parent(Y,X).
+
+%Checks if X is the grandson of Y
+grand_so(X,Y):- parent(Y,Z), son(X,Z).
+
+%Predicate that outputs all the grandsons of X
+grand_sons(X):- grand_so(Y,X), print(Y), nl, fail.
+
+%Predicate that checks whether X and Y are grandmother and grandson or grandson and grandmother.
+grand_ma_and_son(X,Y):-
+	parent(X,Z), parent(Z,Y), woman(X), man(Y), print(yes), nl, fail;
+	parent(Y,Z), parent(Z,X), woman(Y), man(X), print(yes), nl, fail.
+
+%Checks if X is the brother of Y
+brother(X,Y):- parent(Z,X), parent(Z,Y), man(X), man(Z), print(yes), nl, fail.
+
+%Checks if X is the uncle of Y
+uncle(X,Y):- parent(Z,Y), brother(X,Z), nl, fail.
+
+%Predicate that outputs all the uncles of X
+uncle(X):- uncle(Y,X), print(Y), nl, fail.
